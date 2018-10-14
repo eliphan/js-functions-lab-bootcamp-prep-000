c6cff04d93599fca1c0aa1f82c3835af993e0508
generic-thread-7687 <aliceconnic@gmail.com>
2018-10-14 00:04:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 // write your code below!
-function (happyHolidays) {
+function happyHolidays() {
   return "Happy holidays!"
 }
+
+function happyHolidaysTo (name) {
+  return "Happy holidays, ${name}"
+}
+function happyHolidayTo (holiday,name) {
